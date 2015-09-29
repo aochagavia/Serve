1ea6f8d1542a5230404bfa21b14f6887e8613b1c
Adolfo Ochagavía <aochagavia92@gmail.com>
2015-09-29 16:13:10 +0200

    Style fixes

--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,13 @@ extern crate iron;
 extern crate staticfile;
 
 use iron::Iron;
-
 use staticfile::Static;
-
 use std::env;
 
+const DEFAULT_PORT: &'static str = "8080";
+
 fn main() {
-    let default_port = "8080".to_string();
-    let port = env::args().nth(1).unwrap_or(default_port);
+    let port = env::args().nth(1).unwrap_or(DEFAULT_PORT.into());
     let addr = format!("localhost:{}", port);
 
     match Iron::new(Static::new(".")).http(&*addr) {
@@ -17,7 +16,9 @@ fn main() {
             println!("Listening on port {}", port);
         },
         Err(_) => {
-            println!("Could not start server on port {}. Do you have permission or is it already in use?", port);
+            println!("Could not start server on port {}. \
+                      Make sure that you have the required permissions \
+                      and that the port is not already in use.", port);
         },
     }
 }
