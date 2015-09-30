4e488b3d1521366f2756d81e54d2678c91a5ec85
baby-bell <baby-bell@users.noreply.github.com>
2015-09-29 17:15:35 -0700

    added argparse for help and argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,27 @@
 extern crate iron;
 extern crate staticfile;
+extern crate argparse;
 
 use iron::Iron;
 use staticfile::Static;
-use std::env;
+use argparse::{ArgumentParser, Store};
 
 const DEFAULT_PORT: &'static str = "8080";
 
 fn main() {
-    let port = env::args().nth(1).unwrap_or(DEFAULT_PORT.into());
+    let mut port = "".to_string();
+    {
+        let mut parser = ArgumentParser::new();
+        parser.set_description("Serve files in the current directory via HTTP.");
+
+        parser.refer(&mut port)
+            .add_argument("port", Store,
+            "the port to listen on. Defaults to 8080");
+
+        parser.parse_args_or_exit();
+    }
+
+    let port = if port == "" {DEFAULT_PORT.to_string()} else {port};
     let addr = format!("localhost:{}", port);
 
     match Iron::new(Static::new(".")).http(&*addr) {
