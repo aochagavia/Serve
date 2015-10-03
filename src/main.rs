8140d3e9773208536872f15f98dc513646526a91
baby-bell <baby-bell@users.noreply.github.com>
2015-10-02 22:06:13 -0700

    add docopts for argument parsing

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,32 @@
 extern crate iron;
 extern crate staticfile;
+extern crate docopt;
 
 use iron::Iron;
 use staticfile::Static;
-use std::env;
+use docopt::Docopt;
 
 const DEFAULT_PORT: &'static str = "8080";
+const USAGE: &'static str = "
+Serve files in the current directory via HTTP.
+
+Usage: serve [<port>]
+       serve (-h | --help)
+
+Options:
+    -h, --help  Show this screen";
 
 fn main() {
-    let port = env::args().nth(1).unwrap_or(DEFAULT_PORT.into());
+    let args = Docopt::new(USAGE)
+                      .and_then(|dopt| dopt.parse())
+                      .unwrap_or_else(|e| e.exit());
+
+    let port = if args.get_str("<port>") != "" {
+        args.get_str("<port>").to_string()
+    } else {
+        DEFAULT_PORT.to_string()
+    };
+
     let addr = format!("localhost:{}", port);
 
     match Iron::new(Static::new(".")).http(&*addr) {
